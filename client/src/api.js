aa44e74291504ee6ea753c59a06b35d8bd463282
FurkannnAtes <aliriza20005@hotmail.com>
2023-02-18 01:54:36 +0300

    second commit

--- client/src/api.js
+++ client/src/api.js
@@ -1,14 +1,17 @@
 export const fetchResponse = async (chat) => {
   try {
-    const response = await fetch("http://localhost:3080/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        message: chat.map((message) => message.message).join(" \n "),
-      }),
-    });
+    const response = await fetch(
+      "https://chatgpt-clone-backend-o4x3.onrender.com",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: chat.map((message) => message.message).join(" \n "),
+        }),
+      }
+    );
     const data = await response.json();
     return data;
   } catch (error) {
